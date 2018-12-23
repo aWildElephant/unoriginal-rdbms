ac61a93aeb5cd7b34004f086e746be0afd925525
Etienne Girard <etienne.g.girard@gmail.com>
2018-12-23 18:39:05 +0100

    Removed duplicated Ignore annotation after merge

--- feature-tests/src/test/java/fr/awildelephant/rdbms/features/DebugCucumberTest.java
+++ feature-tests/src/test/java/fr/awildelephant/rdbms/features/DebugCucumberTest.java
@@ -5,9 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
-@Ignore
+@Ignore // TODO: this test could be use to make the build fail if a feature test marked as todo passes
 @CucumberOptions(glue = {"fr.awildelephant.rdbms.features"}, tags = "@debug")
 @RunWith(Cucumber.class)
-@Ignore // TODO: this test could be use to make the build fail if a feature test marked as todo passes
 public class DebugCucumberTest {
 }
