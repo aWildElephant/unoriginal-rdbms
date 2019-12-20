ce511d818593eac50d51a752d2bb0238d64877f0
Etienne Girard <etienne.g.girard@gmail.com>
2019-12-20 19:22:57 +0100

    Fix typo

--- engine/optimizer/src/main/java/fr/awildelephant/rdbms/engine/optimizer/Optimizer.java
+++ engine/optimizer/src/main/java/fr/awildelephant/rdbms/engine/optimizer/Optimizer.java
@@ -7,7 +7,6 @@ import fr.awildelephant.rdbms.plan.LogicalOperator;
 public class Optimizer {
 
     public LogicalOperator optimize(LogicalOperator plan) {
-        final LogicalOperator moncul = new SubqueryUnnesting().apply(plan);
-        return new FilterPushDown().apply(moncul);
+        return new FilterPushDown().apply(new SubqueryUnnesting().apply(plan));
     }
 }
