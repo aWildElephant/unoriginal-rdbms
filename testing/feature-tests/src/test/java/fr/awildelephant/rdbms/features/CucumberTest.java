52f7973c442589e2628b3080d3f058a4560d4f96
Etienne Girard <etienne.g.girard@gmail.com>
2020-05-09 18:29:47 +0200

    Update cucumber and equalsverifier

--- testing/feature-tests/src/test/java/fr/awildelephant/rdbms/features/CucumberTest.java
+++ testing/feature-tests/src/test/java/fr/awildelephant/rdbms/features/CucumberTest.java
@@ -1,7 +1,7 @@
 package fr.awildelephant.rdbms.features;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @CucumberOptions(glue = {"fr.awildelephant.rdbms.features"}, tags = "not @todo")
