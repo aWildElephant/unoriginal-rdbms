e5437e5466de87760982ce8c61021327ca405fbb
Étienne Girard <etienne.g.girard@gmail.com>
2020-06-09 00:24:00 +0200

    Add TPC-H Q16 feature test

--- testing/tpch-tests/src/test/java/fr/awildelephant/rdbms/tpch/StepDefs.java
+++ testing/tpch-tests/src/test/java/fr/awildelephant/rdbms/tpch/StepDefs.java
@@ -1,23 +1,25 @@
 package fr.awildelephant.rdbms.tpch;
 
-import io.cucumber.java8.En;
 import fr.awildelephant.csvloader.Loader;
 import fr.awildelephant.rdbms.test.commons.ExpectedResult;
 import fr.awildelephant.rdbms.test.commons.RDBMSTestWrapper;
 import io.cucumber.datatable.DataTable;
+import io.cucumber.java8.En;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Paths;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static fr.awildelephant.rdbms.test.commons.ResultSetAsserter.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -64,7 +66,29 @@ public class StepDefs implements En {
 
         When("^I execute the query$", testWrapper::execute);
 
-        Then("^I expect the result$", this::assertResult);
+        Then("^I expect the result$", (DataTable dataTable) -> assertResult(dataTable.asLists()));
+
+        Then("^I expect the result described in (.+)$", fileName -> {
+            final InputStream inputStream = this.getClass().getResourceAsStream("/results/" + fileName);
+
+            final CSVParser parser = CSVFormat.DEFAULT.parse(new BufferedReader(new InputStreamReader(inputStream)));
+
+            final List<List<String>> expectedResult = parser.getRecords().stream()
+                    .map(this::toList)
+                    .collect(Collectors.toList());
+
+            assertResult(expectedResult);
+        });
+    }
+
+    private List<String> toList(CSVRecord record) {
+        final List<String> list = new ArrayList<>(record.size());
+
+        for (String item : record) {
+            list.add(item);
+        }
+
+        return list;
     }
 
     private String createTableQuery(String tableName) {
@@ -80,14 +104,12 @@ public class StepDefs implements En {
         }
     }
 
-    private void assertResult(DataTable table) throws Exception {
+    private void assertResult(List<List<String>> expectedResult) throws Exception {
         testWrapper.forwardExceptionIfPresent();
 
         final ResultSet lastResult = testWrapper.getStatement().getResultSet();
 
         assertNotNull(lastResult, "Result set is null: no query run or last query was an update");
-
-        final List<List<String>> expectedResult = table.asLists();
 
         final List<String> expectedColumnNames = expectedResult.get(0);
         final List<String> expectedColumnTypes = expectedResult.get(1);
