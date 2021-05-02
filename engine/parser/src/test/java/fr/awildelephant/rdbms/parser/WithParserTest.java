1aee6f2aa05ed677ae9a74c2be789e4d2c2a3212
Etienne Girard <etienne.g.girard@gmail.com>
2021-05-02 14:05:28 +0200

    Simplify parser test for with statement with single element

--- engine/parser/src/test/java/fr/awildelephant/rdbms/parser/WithParserTest.java
+++ engine/parser/src/test/java/fr/awildelephant/rdbms/parser/WithParserTest.java
@@ -5,14 +5,11 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static fr.awildelephant.rdbms.ast.Asterisk.asterisk;
-import static fr.awildelephant.rdbms.ast.QualifiedColumnName.qualifiedColumnName;
 import static fr.awildelephant.rdbms.ast.Select.select;
 import static fr.awildelephant.rdbms.ast.TableName.tableName;
-import static fr.awildelephant.rdbms.ast.TableReferenceList.tableReferenceList;
 import static fr.awildelephant.rdbms.ast.With.with;
 import static fr.awildelephant.rdbms.ast.WithElement.withElement;
 import static fr.awildelephant.rdbms.ast.WithList.withList;
-import static fr.awildelephant.rdbms.ast.value.Equal.equal;
 import static fr.awildelephant.rdbms.parser.ParserTestHelper.assertParsing;
 import static fr.awildelephant.rdbms.parser.ParserTestHelper.columns;
 
@@ -20,7 +17,7 @@ public class WithParserTest {
 
     @Test
     void it_should_parse_a_with_statement_with_a_single_element() {
-        assertParsing("WITH with_query AS (SELECT a, b FROM t1) SELECT * FROM t2, with_query WHERE t2.a = with_query.a",
+        assertParsing("WITH with_query AS (SELECT a, b FROM t1) SELECT * FROM with_query",
                       with(withList(List.of(withElement("with_query", select(columns("a", "b"),
                                                                              tableName("t1"),
                                                                              null,
@@ -28,8 +25,8 @@ public class WithParserTest {
                                                                              null,
                                                                              null)))),
                            select(List.of(asterisk()),
-                                  tableReferenceList(tableName("t2"), tableName("with_query"), List.of()),
-                                  equal(qualifiedColumnName("t2", "a"), qualifiedColumnName("with_query", "a")),
+                                  tableName("with_query"),
+                                  null,
                                   null,
                                   null,
                                   null)));
