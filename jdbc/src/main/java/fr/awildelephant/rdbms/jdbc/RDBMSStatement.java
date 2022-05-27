fd8fbbb394694d3d1de7782aecc5090b6d2e43e1
Étienne Girard <etienne.g.girard@gmail.com>
2022-05-27 15:27:51 +0200

    Remove obscure TODO

--- jdbc/src/main/java/fr/awildelephant/rdbms/jdbc/RDBMSStatement.java
+++ jdbc/src/main/java/fr/awildelephant/rdbms/jdbc/RDBMSStatement.java
@@ -29,7 +29,7 @@ public class RDBMSStatement extends AbstractStatement {
 
         closeLastResultSet();
 
-        lastResult = new RDBMSResultSet( /* TODO */ table);
+        lastResult = new RDBMSResultSet(table);
 
         return true;
     }
