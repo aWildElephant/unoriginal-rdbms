7641aa32e4b835d011bcdb467d28fbd960cc10fb
Étienne Girard <etienne.g.girard@gmail.com>
2023-05-02 20:12:00 +0200

    Bump java version to 20

--- engine/glue/src/main/java/fr/awildelephant/rdbms/server/Glue.java
+++ engine/glue/src/main/java/fr/awildelephant/rdbms/server/Glue.java
@@ -116,8 +116,8 @@ public final class Glue {
         }
 
         @Provides
-        QueryDispatcherOld queryDispatcher(Storage storage, Algebraizer algebraizer, Optimizer optimizer, WithInlinerFactory withInlinerFactory, LogicalPlanTableBuilder logicalPlanTableBuilder, PlanFactory planFactory) {
-            return new QueryDispatcherOld(storage, algebraizer, optimizer, withInlinerFactory, logicalPlanTableBuilder, planFactory);
+        QueryDispatcher queryDispatcher(Storage storage) {
+            return new QueryDispatcher(storage);
         }
 
         @Provides
