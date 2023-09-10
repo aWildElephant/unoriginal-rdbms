f229e3d7b70317fc439ef60db56f0c8b89fd1cfb
Étienne Girard <etienne.g.girard@gmail.com>
2023-09-10 20:08:45 +0200

    Remove unused constant

--- engine/schema/src/main/java/fr/awildelephant/rdbms/schema/ReservedKeywords.java
+++ engine/schema/src/main/java/fr/awildelephant/rdbms/schema/ReservedKeywords.java
@@ -8,5 +8,4 @@ public final class ReservedKeywords {
 
     public static final String FROM_VERSION_COLUMN = "<from>";
     public static final String TO_VERSION_COLUMN = "<to>";
-    static final String SYSTEM_TABLE = "<mvcc>";
 }
