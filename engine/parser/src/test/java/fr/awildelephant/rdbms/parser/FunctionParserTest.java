529beeb834abfb63712ccfdce163824e8a065577
Étienne Girard <etienne.g.girard@gmail.com>
2023-12-27 17:17:51 +0100

    Add disabled test for parsing function in SELECT output columns

--- engine/parser/src/test/java/fr/awildelephant/rdbms/parser/FunctionParserTest.java
+++ engine/parser/src/test/java/fr/awildelephant/rdbms/parser/FunctionParserTest.java
@@ -1,5 +1,6 @@
 package fr.awildelephant.rdbms.parser;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -7,6 +8,8 @@ import java.util.List;
 import static fr.awildelephant.rdbms.ast.Asterisk.asterisk;
 import static fr.awildelephant.rdbms.ast.Function.function;
 import static fr.awildelephant.rdbms.ast.Select.select;
+import static fr.awildelephant.rdbms.ast.TableName.tableName;
+import static fr.awildelephant.rdbms.ast.value.IntegerLiteral.integerLiteral;
 import static fr.awildelephant.rdbms.ast.value.TextLiteral.textLiteral;
 import static fr.awildelephant.rdbms.parser.ParserTestHelper.assertParsing;
 
@@ -17,5 +20,13 @@ class FunctionParserTest {
         assertParsing("SELECT * FROM csv('/home/etienne/test.csv')",
 
                 select(List.of(asterisk()), function("csv", List.of(textLiteral("/home/etienne/test.csv"))), null, null, null, null));
+    }
+
+    @Disabled // TODO
+    @Test
+    void it_should_parse_a_function_in_the_output_columns_of_a_select_query() {
+        assertParsing("SELECT complex_computation(0, 0) FROM la_tete_a_toto",
+
+                select(List.of(function("complex_computation", List.of(integerLiteral(0), integerLiteral(0)))), tableName("la_tete_a_toto"), null, null, null, null));
     }
 }
