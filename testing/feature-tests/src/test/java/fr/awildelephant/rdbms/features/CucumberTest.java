4f500fb16ac68df98cac742090eb4360d07d428f
Etienne Girard <etienne.g.girard@gmail.com>
2026-03-12 21:26:30 +0100

    Exclude tag todo again

--- testing/feature-tests/src/test/java/fr/awildelephant/rdbms/features/CucumberTest.java
+++ testing/feature-tests/src/test/java/fr/awildelephant/rdbms/features/CucumberTest.java
@@ -1,5 +1,6 @@
 package fr.awildelephant.rdbms.features;
 
+import org.junit.platform.suite.api.ExcludeTags;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
@@ -7,5 +8,6 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectPackages("fr.awildelephant.rdbms.features")
+@ExcludeTags("todo")
 public class CucumberTest {
 }
